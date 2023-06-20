7d8796181cfcf80fc049dae58c5e28e32dfbda4d
Abigail Howell <aahowel3@asu.edu>
2023-06-20 11:45:15 -0700

    confirm and explore upset

--- upsetRforcomparitivepaper.R
+++ upsetRforcomparitivepaper.R
@@ -1,41 +1,89 @@
-setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/")
+setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/")
 
-virhostmatcher=read.csv("virhostmatcher_gordonia_paper.melted.shape.csv")
+virhostmatcher=read.csv("virhostmatcher_gordonia_paper.melted.csv")
 phirbo=read.csv("phirbo_gordonia_paper.list.matrix.melted.csv")
-wish=read.csv("wish_gordonia_paper.shape.pvalue.csv")
+wish=read.csv("prediction.list",sep="\t")
+
+
+#hy01 specific
+####use these modified files with the variable name column cahnged so it matches the other files 
+virhostmatcher=read.csv("hy01_virhostmatcher_gordonia_paper.melted.upset.csv")
+phirbo=read.csv("hy01_phirbo_gordonia_paper.list.matrix.melted.upset.csv")
+wish=read.csv("hy01_wish_prediction.upset.list",sep="\t")
+
+#allviruses together specific
+####use these modified files with the variable name column cahnged so it matches the other files 
+virhostmatcher=read.csv("virhostmatcher_allviruses.csv")
+phirbo=read.csv("phirbo_allviruses.csv")
+wish=read.csv("wish_allviruses.csv")
+php=read.csv("php_allviruses.csv")
+
+
+setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/WISH/formanuscript/SFP10/")
+all=read.csv("SFP10_confirmatory.csv")
+
+library(data.table)
+virhostmatcher=all[all$tool == "VHM",]
+phirbo=all[all$tool == "Phirbo",]
+php=all[all$tool == "PHP",]
+wish=all[all$tool == "WIsH",]
+
+#only GMAS/GRUS
+virhostmatcher=all[all$tool == "VHM" & all$virus %like% "G",]
+phirbo=all[all$tool == "Phirbo" & all$virus %like% "G",]
+php=all[all$tool == "PHP" & all$virus %like% "G",]
+wish=all[all$tool == "WIsH" & all$virus %like% "G",]
+
+#only ecoli virus
+virhostmatcher=all[all$tool == "VHM" & all$virus %like% "KFS|SFP|HY",]
+phirbo=all[all$tool == "Phirbo" & all$virus %like% "KFS|SFP|HY",]
+php=all[all$tool == "PHP" & all$virus %like% "KFS|SFP|HY",]
+wish=all[all$tool == "WIsH" & all$virus %like% "KFS|SFP|HY",]
+
 
 library(dplyr)
 virhostmatcher <- virhostmatcher %>%
-  mutate(category = case_when(value < 0.175 & shape == 16 ~ 'TP',
-                              value < 0.175 & shape == 15 ~ 'FP',
-                              value > 0.175 & shape == 15 ~ 'TN',
-                              value > 0.175 & shape == 16 ~ 'FN'))
+  mutate(category = case_when(score < 0.175 & shape == 16 ~ 'TP',
+                              score < 0.175 & shape == 15 ~ 'FP',
+                              score > 0.175 & shape == 15 ~ 'TN',
+                              score > 0.175 & shape == 16 ~ 'FN'))
 
 phirbo <- phirbo %>%
-  mutate(category = case_when(value > 0.2 & shape == 16 ~ 'TP',
-                              value > 0.2 & shape == 15 ~ 'FP',
-                              value < 0.2 & shape == 15 ~ 'TN',
-                              value < 0.2 & shape == 16 ~ 'FN'))
+  mutate(category = case_when(score > 0.2 & shape == 16 ~ 'TP',
+                              score > 0.2 & shape == 15 ~ 'FP',
+                              score < 0.2 & shape == 15 ~ 'TN',
+                              score < 0.2 & shape == 16 ~ 'FN'))
 
 wish <- wish %>%
-  mutate(category = case_when(p.value.if.null.parameters.provided < 0.06 & shape == 16 ~ 'TP',
-                              p.value.if.null.parameters.provided < 0.06 & shape == 15 ~ 'FP',
-                              p.value.if.null.parameters.provided > 0.06 & shape == 15 ~ 'TN',
-                              p.value.if.null.parameters.provided > 0.06 & shape == 16 ~ 'FN'))
+  mutate(category = case_when(pvalue < 0.06 & shape == 16 ~ 'TP',
+                              pvalue < 0.06 & shape == 15 ~ 'FP',
+                              pvalue > 0.06 & shape == 15 ~ 'TN',
+                              pvalue > 0.06 & shape == 16 ~ 'FN'))
+
+php <- php %>%
+  mutate(category = case_when(score > 1442 & shape == 16 ~ 'TP',
+                              score > 1442 & shape == 15 ~ 'FP',
+                              score < 1442 & shape == 15 ~ 'TN',
+                              score < 1442 & shape == 16 ~ 'FN'))
+
 
 #calculate sensitivity and specificity for each tool 
 table(virhostmatcher$category)
 table(phirbo$category)
 table(wish$category)
+table(php$category)
+
 
 
 library(stringr)
 library(ComplexHeatmap)
-set1 <- str_c(virhostmatcher$X,"_", virhostmatcher$variable,"_type_",virhostmatcher$category)
-set2 <- str_c(phirbo$variable,"_", phirbo$X,"_type_",phirbo$category)
-set3 <- str_c(wish$Phage,"_", wish$Best.hit.among.provided.hosts,"_type_",wish$category)
+set1 <- str_c(virhostmatcher$virus,"_", virhostmatcher$host,"_type_",virhostmatcher$category)
+set2 <- str_c(phirbo$virus,"_", phirbo$host,"_type_",phirbo$category)
+set3 <- str_c(wish$virus,"_", wish$host,"_type_",wish$category)
+set4 <- str_c(php$virus,"_", php$host,"_type_",php$category)
 
-lt=list(set1,set2,set3)
+
+lt=list(set1,set2,set3,set4)
 set_matrix=list_to_matrix(lt)
 qwerty=as.data.frame(set_matrix)
 library(tibble)
@@ -45,14 +93,107 @@ library(tidyr)
 qwerty = qwerty %>%
   separate(VALUE, c("foo", "bar"), "_type_")
 
-colnames(qwerty) = c("virus-host interaction","Accuracy","VirHostMatcher","Phirbo","WIsH")
-interactions = colnames(qwerty)[3:5]
+colnames(qwerty) = c("virus-host interaction","Accuracy","VirHostMatcher","Phirbo","WIsH","PHP")
+interactions = colnames(qwerty)[3:6]
 library(ComplexUpset)
 library(ggplot2)
-upset(
+ComplexUpset::upset(
   qwerty,
   interactions,
   name='Confirmatory Tools',
+  sort_intersections_by=c('degree', 'cardinality'),
+set_sizes=FALSE,
+  intersections='all',
+  base_annotations=list(
+    'Intersection size'=intersection_size(
+      counts=FALSE,
+      mapping=aes(fill=Accuracy)
+    )
+  ),
+  width_ratio=0.1 
+)
+
+
+#####
+###########################################
+############################
+######
+library(stringr)
+library(ComplexHeatmap)
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(ComplexUpset)
+library(ggplot2)
+        
+#exploratroy tools upset
+setwd("C:/Users/Owner/OneDrive - Arizona State University/Documents/hostrange/")
+all=read.csv("exploratoryresults_upset.csv")
+
+#have to have them seperated by tool then glue back together
+cherry=all[all$tool == "CHERRY",]
+vhmn=all[all$tool == "VHMN",]
+set1 <- str_c(cherry$virus,"_", cherry$host,"_type_",cherry$category)
+set2 <- str_c(vhmn$virus,"_", vhmn$host,"_type_",vhmn$category)
+lt=list(set1,set2)
+set_matrix=list_to_matrix(lt)
+qwerty=as.data.frame(set_matrix)
+qwerty <- tibble::rownames_to_column(qwerty, "VALUE")
+qwerty = qwerty %>%
+  separate(VALUE, c("foo", "bar"), "_type_")
+
+colnames(qwerty) = c("virus-host interaction","Accuracy","CHERRY","VHMN")
+interactions = colnames(qwerty)[3:4]
+library(ComplexUpset)
+library(ggplot2)
+ComplexUpset::upset(
+  qwerty,
+  interactions,
+  name='Confirmatory Tools',
+  sort_intersections_by=c('degree', 'cardinality'),
+  set_sizes=FALSE,
+  base_annotations=list(
+    'Intersection size'=intersection_size(
+      counts=FALSE,
+      mapping=aes(fill=Accuracy)
+    )
+  ),
+  width_ratio=0.1 
+)
+
+
+
+
+
+
+
+
+hostg=all[all$tool == "HostG",]
+rafah=all[all$tool == "RaFAH",]
+vhulk=all[all$tool == "vHULK",]
+vpfclass=all[all$tool == "vpf-class",]
+
+set3 <- str_c(hostg$virus,"_", hostg$host,"_type_",hostg$category)
+set4 <- str_c(rafah$virus,"_", rafah$host,"_type_",rafah$category)
+set5 <- str_c(vhulk$virus,"_", vhulk$host,"_type_",vhulk$category)
+set6 <- str_c(vpfclass$virus,"_", vpfclass$host,"_type_",vpfclass$category)
+
+lt=list(set3,set4,set5,set6)
+set_matrix=list_to_matrix(lt)
+qwerty=as.data.frame(set_matrix)
+qwerty <- tibble::rownames_to_column(qwerty, "VALUE")
+qwerty = qwerty %>%
+  separate(VALUE, c("foo", "bar"), "_type_")
+
+colnames(qwerty) = c("virus-host interaction","Accuracy","HostG","RaFAH","vHULK","vpf-class")
+interactions = colnames(qwerty)[3:6]
+library(ComplexUpset)
+library(ggplot2)
+ComplexUpset::upset(
+  qwerty,
+  interactions,
+  name='Confirmatory Tools',
+  sort_intersections_by=c('degree', 'cardinality'),
   set_sizes=FALSE,
   base_annotations=list(
     'Intersection size'=intersection_size(
